cede44ac2220d9b40554eb1d7dd3d8bebf41259b
xensky <xensky@gmail.com>
2014-02-22 16:19:03 -0500

    restarted with improved methods to match and parse commands

---
+++ src/thy_dungeonman/command.clj
@@ -0,0 +1,37 @@
+(ns thy-dungeonman.command)
+
+; commands are given in a context-free grammar format
+;   :get-ye-flask > :get :ye "flask"
+;   :get > "get" | "take"
+;   :ye > "ye" | "yon" | nil
+; :keywords are used as non-terminal symbols, "strings" are terminal symbols
+
+(def | '|)
+
+(defn make-command
+  "converts a pretty representation of a cfg rule into processable data"
+  [symbol _ & productions]
+  (let [rules (filter #(not= '(|) %) (partition-by #(= | %) productions))]
+    {symbol rules}))
+
+(defn match-command
+  "attempts to match an input to a command's rules"
+  [input commands start]
+  (letfn [(expand [input symbol stack]
+            (reduce #(or %1 %2)
+                    (map #(match input (concat % stack))
+                         (get commands symbol))))
+          (match [input stack]
+            (let [empty-input? (empty? input)
+                  empty-stack? (empty? stack)
+                  top-input (first input)
+                  top-stack (first stack)
+                  rest-input (rest input)
+                  rest-stack (rest stack)]
+              (cond
+               (and empty-input? empty-stack?) true ; both empty
+               (or empty-input? empty-stack?) false ; one empty
+               (= top-input top-stack) (match rest-input rest-stack) ; match
+               (keyword? top-stack) (expand input top-stack rest-stack) ; expand
+               :else false)))]
+    (match input [start])))
--- src/thy_dungeonman/core.clj
+++ src/thy_dungeonman/core.clj
@@ -1,7 +1,13 @@
 (ns thy-dungeonman.core
-  (:gen-class))
+  (:gen-class)
+  (:use [thy-dungeonman.command :only [make-command match-command |]]))
 
 (defn -main
   "initialize game stuffs and start running"
   [& args]
-  (println "game goes here"))
+  (let [commands (merge
+                  (make-command :get-ye-flask > :get :ye "flask")
+                  (make-command :get > "get" | "take")
+                  (make-command :ye > "ye" | "yon"))
+        matched (match-command ["get" "ye" "flask"] commands :get-ye-flask)]
+    (println (str "match get ye flask: " matched))))
