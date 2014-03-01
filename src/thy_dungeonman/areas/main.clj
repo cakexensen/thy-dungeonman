e6d58a0281f35b88e562a6dbf92b2246773ac809
xensky <xensky@gmail.com>
2014-03-01 13:07:16 -0500

    add move handler, updates the location of the game

--- src/thy_dungeonman/areas/main.clj
+++ src/thy_dungeonman/areas/main.clj
@@ -1,14 +1,17 @@
 (ns thy-dungeonman.areas.main
   (:use [thy-dungeonman.areas.core :only [->Area]]
         [thy-dungeonman.command :only [make-command]]
-        [thy-dungeonman.handler :only [message]]))
+        [thy-dungeonman.handler :only [message move]]))
 
 (def commands
-  (merge (make-command :look-flask -> "look" "flask")))
+  (merge (make-command :look-flask -> "look" "flask")
+         (make-command :go-north -> "go" "north")))
 
 (def handlers
   {:look-flask (fn [game unknowns]
-                 (message game "Looks like you could quaff some serious mead out of that thing."))})
+                 (message game "Looks like you could quaff some serious mead out of that thing."))
+   :go-north (fn [game unknowns]
+               (move game :north))})
 
 (defn make-main
   "creates the initial main room"
--- src/thy_dungeonman/handler.clj
+++ src/thy_dungeonman/handler.clj
@@ -4,3 +4,8 @@
   "sets the message to display"
   [game text]
   (assoc game :message text))
+
+(defn move
+  "moves the player to a new location"
+  [game location]
+  (assoc game :location location))
