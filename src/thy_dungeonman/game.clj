b7acb797ce65826616350bb06fc38792045a59e5
xensky <xensky@gmail.com>
2014-03-08 11:09:09 -0500

    add score handler, add generic commands and handlers to game

--- src/thy_dungeonman/game.clj
+++ src/thy_dungeonman/game.clj
@@ -1,6 +1,7 @@
 (ns thy-dungeonman.game
   (:use [thy-dungeonman.areas.main :only [make-main]]
-        [thy-dungeonman.command :only [make-command]]))
+        [thy-dungeonman.command :only [make-command]]
+        [thy-dungeonman.handler :only [message score]]))
 
 (defrecord Game [location dictionary areas commands handlers message])
 
@@ -12,9 +13,43 @@
 
 (def areas {:main (make-main)})
 
-(def commands {})
+(def commands
+  (merge (make-command :die -> "die")
+         (make-command :dance -> "dance")
+         (make-command :get-unknown -> :get :unknown)
+         (make-command :get-dagger -> :get "dagger")
+         (make-command :go-unknown -> "go" :unknown)
+         (make-command :look-unknown -> "look" :unknown)
+         (make-command :talk-unknown -> "talk" :unknown)
+         (make-command :give-unknown -> "give" :unknown)))
 
-(def handlers {})
+(def handlers
+  {:die (fn [game unknowns]
+          (-> game
+              (score -100)
+              (message "That wasn't very smart. Your score was "
+                       (:score game)
+                       ". Play again? [Y/N]")))
+   :dance (fn [game unknowns]
+            (message game "Thou shaketh it a little, and it feeleth all right."))
+   :get-unknown (fn [game unknowns]
+                  (message game "Thou cannotst get that. Quit making stuffeth up!"))
+   :get-dagger (fn [game unknowns]
+                 (-> game
+                     (message "Yeah, okay.")
+                     (score 25)))
+   :go-unknown (fn [game unknowns]
+                 (message game "Thou cannotst go there. Who do you think thou art, a magistrate?!"))
+   :look-unknown (fn [game unknowns]
+                   (message game "It looketh pretty awesome."))
+   :unknown (fn [game unknowns]
+              (message game "That does not computeth. Type HELP if thou needs of it."))
+   :talk-unknown (fn [game unknowns]
+                   (message game "Who is " (apply str unknowns) "? Your new boyfriend? Somebody from work you don't want me to meeteth?"))
+   :give-unknown (fn [game unknowns]
+                   (message game "Thou don'tst have a " unknowns " to give. Go back to your tiny life."))
+   :smell (fn [game unknowns]
+            (message game "You smell a Wumpus."))})
 
 (defn new-game
   "initializes a new game"
--- src/thy_dungeonman/handler.clj
+++ src/thy_dungeonman/handler.clj
@@ -2,10 +2,15 @@
 
 (defn message
   "sets the message to display"
-  [game text]
-  (assoc game :message text))
+  [game & text]
+  (assoc game :message (apply str text)))
 
 (defn move
   "moves the player to a new location"
   [game location]
   (assoc game :location location))
+
+(defn score
+  "updates the game score"
+  [game delta]
+  (update-in game [:score] #(+ % delta)))
