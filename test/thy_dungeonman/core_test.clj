0cff8006af7564a07e27d549f124a143b675a9ff
xensky <xensky@gmail.com>
2014-03-08 11:28:16 -0500

    finally add some unit testing

--- test/thy_dungeonman/core_test.clj
+++ test/thy_dungeonman/core_test.clj
@@ -1,7 +1,17 @@
 (ns thy-dungeonman.core-test
   (:require [clojure.test :refer :all]
-            [thy-dungeonman.core :refer :all]))
+            [thy-dungeonman.core :refer :all]
+            [thy-dungeonman.game :refer [new-game]]
+            [thy-dungeonman.command :refer [process-input]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest a-new-game
+  (testing "game starts in main room"
+    (is (= (:location (new-game)) :main)))
+  (testing "game dispatches generic command"
+    (is (= (:message (process-input "look foot" (new-game)))
+           "It looketh pretty awesome."))))
+
+(deftest main-room
+  (testing "look flask message in main room"
+    (is (= (:message (process-input "look flask" (new-game)))
+           "Looks like you could quaff some serious mead out of that thing."))))
