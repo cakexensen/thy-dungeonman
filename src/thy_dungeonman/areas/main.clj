3eb25d35e6666af46003995aa1739ea4522e5c25
xensky <xensky@gmail.com>
2014-03-08 13:44:37 -0500

    add more commands to main room, add test apparatus for simulating multiple inputs of text

--- src/thy_dungeonman/areas/main.clj
+++ src/thy_dungeonman/areas/main.clj
@@ -1,17 +1,46 @@
 (ns thy-dungeonman.areas.main
   (:use [thy-dungeonman.areas.core :only [->Area]]
         [thy-dungeonman.command :only [make-command]]
-        [thy-dungeonman.handler :only [message move]]))
+        [thy-dungeonman.handler :only [message move score]]))
 
 (def commands
-  (merge (make-command :look-flask -> "look" "flask")
+  (merge (make-command :look-flask -> "look" :ye "flask")
+         (make-command :look-scroll -> "look" :ye "scroll")
+         (make-command :get-flask -> :get :ye "flask")
+         (make-command :get-scroll -> :get :ye "scroll")
          (make-command :go-north -> "go" "north")))
 
 (def handlers
   {:help (fn [game unknowns]
-           (message game "Ye find yeself in yon dungeon. Ye see a SCROLL. Behind ye scroll is a FLASK. Obvious exits are NORTH, SOUTH and DENNIS."))
+           (message game "Ye find yeself in yon dungeon. Ye see a SCROLL. Behind ye scroll is a FLASK. Obvious exits are NORTH, SOUTH and DENNIS."
+                    (when (get-in game [:areas :main :scroll-gone])
+                      " There is definitely no YE SCROLL, so drop it.")))
    :look-flask (fn [game unknowns]
                  (message game "Looks like you could quaff some serious mead out of that thing."))
+   :look-scroll (fn [game unknowns]
+                  (if (get-in game [:areas :main :scroll-gone])
+                    (message game "Ye seeth nothing wheretofore it went ZAP.")
+                    (message game "Parchment, definitely parchment. I'd recognize it anywhere.")))
+   :get-flask (fn [game unknowns]
+                (if (< (get-in game [:areas :main :get-flask-count] 0) 3)
+                  (-> game
+                      (message "Ye cannot get the FLASK. It is firmly bolted to a wall which is bolted to the rest of the dungeon which is probably bolted to a castle. Never you mind.")
+                      (score 1)
+                      (update-in [:areas :main :get-flask-count] (fnil inc 1)))
+                  (-> game
+                      (score -1000)
+                      (message "Okay, okay. You unbolt yon FLASK and hold it aloft. A great shaking begins. The dungeon ceiling collapses down on you, crushing you in twain. Apparently, this was a load-bearing FLASK. Your score was: "
+                               (- (:score game) 1000)
+                               " Play again? [Y/N]"))))
+   :get-scroll (fn [game unknowns]
+                 (if (get-in game [:areas :main :scroll-gone])
+                   (-> game
+                       (message "Ye doth suffer from memory loss. YE SCROLL is no more. Honestly.")
+                       (score -1))
+                   (-> game
+                       (message "Ye takes of the scroll and reads of it. It doth say: BEWARE, READER OF THE SCROLL, DANGER AWAITS TO THE- The scroll disappears in thy hands with ye olde ZAP!")
+                       (score 2)
+                       (assoc-in [:areas :main :scroll-gone] true))))
    :go-north (fn [game unknowns]
                (move game :north))})
 
--- test/thy_dungeonman/core_test.clj
+++ test/thy_dungeonman/core_test.clj
@@ -16,10 +16,26 @@
     (is (= (:message (process-input "look flask" (new-game)))
            "Looks like you could quaff some serious mead out of that thing."))))
 
+(defn make-test-inputs
+  "makes a reader function that will input consecutively the values in a list"
+  [commands]
+  (let [index (atom -1)]
+    (fn []
+      (swap! index inc)
+      (get commands @index))))
+
 (deftest game-write-and-read
   (testing "write message"
     (is (let [output-stream (atom "")
               writer (fn [text] (swap! output-stream (fn [_] text)))
-              reader #(str "exit")]
+              reader (make-test-inputs ["exit"])]
           (run-game writer reader)
-          (= @output-stream "Ye find yeself in yon dungeon. Ye see a SCROLL. Behind ye scroll is a FLASK. Obvious exits are NORTH, SOUTH and DENNIS.")))))
+          (= @output-stream "Ye find yeself in yon dungeon. Ye see a SCROLL. Behind ye scroll is a FLASK. Obvious exits are NORTH, SOUTH and DENNIS."))))
+  (testing "read message"
+    (is (let [output-stream (atom "")
+              writer (fn [text] (swap! output-stream (fn [_] text)))
+              reader (make-test-inputs ["get scroll"
+                                        "look scroll"
+                                        "exit"])]
+          (run-game writer reader)
+          (= @output-stream "Ye seeth nothing wheretofore it went ZAP.")))))
